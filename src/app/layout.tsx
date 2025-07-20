957d91e7a40b3b670f155273cd8bf6f0d13fcf0f
Jim2510 <gmguglielmino@gmail.com>
2025-07-20 12:14:03 +0200

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Dak Studio",
@@ -13,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <SpeedInsights/>
       <body className='bg-[#A586C4]'>
         {children}
       </body>
