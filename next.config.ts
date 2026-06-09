2415653207098b34a73a7f2b031d2c92b4cf6e85
Gim2510 <guglielmino2510@gmail.com>
2026-06-09 17:30:19 +0200

    fix en-US redirect

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en-US', 'it'],
-    defaultLocale: 'it',
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/en-US/:path*',
-        destination: '/en/:path*',
-        permanent: true,
-      },
-      {
-        source: '/en-US',
-        destination: '/en',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
